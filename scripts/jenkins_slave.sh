5848f43849a8455d654c3f549a9e2344670e252c
Ramandeep Singh <reyman.raman@gmail.com>
2025-04-25 19:15:58 +0530

    Adding terraform Installation steps

--- scripts/jenkins_slave.sh
+++ scripts/jenkins_slave.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 yum update -y
 yum upgrade -y
+
+# Java Installation
 yum install java-21-amazon-corretto-devel -y
 echo "export JAVA_HOME=/usr/lib/jvm/java-21-amazon-corretto.x86_64" >> /etc/profile
 echo "export PATH=$JAVA_HOME/bin:$PATH" >> /etc/profile
 source /etc/profile
 echo $JAVA_HOME
 #java --version
+
+# Git Installation
 sudo yum install -y git
 sudo hostnamectl set-hostname jenkins-slave
+
+# Terraform Installation
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum install -y terraform
+echo "Terraform installed"
 sudo reboot
