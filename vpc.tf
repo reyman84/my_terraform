24107dfb9e076e236304733f251deb2d75ca4e8f
Ramandeep Singh <reyman.raman@gmail.com>
2025-05-18 12:13:13 +0530

    Updated Resources for Private Subnets

--- vpc.tf
+++ vpc.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "public_subnet_1a" {
   availability_zone       = var.zone["1a"]
   map_public_ip_on_launch = true
   tags = {
-    Name = "Terraform - Public Subnet 1a"
+    Name = "Public Subnet 1a"
   }
 }
 
@@ -35,7 +35,7 @@ resource "aws_subnet" "public_subnet_1b" {
   availability_zone       = var.zone["1b"]
   map_public_ip_on_launch = true
   tags = {
-    Name = "Terraform - Public Subnet 1b"
+    Name = "Public Subnet 1b"
   }
 }
 
@@ -45,7 +45,7 @@ resource "aws_subnet" "public_subnet_1c" {
   availability_zone       = var.zone["1c"]
   map_public_ip_on_launch = true
   tags = {
-    Name = "Terraform - Public Subnet 1c"
+    Name = "Public Subnet 1c"
   }
 }
 
@@ -55,7 +55,7 @@ resource "aws_subnet" "public_subnet_1c" {
   availability_zone       = var.zone["1d"]
   map_public_ip_on_launch = true
   tags = {
-    Name = "Terraform - Public Subnet 1d"
+    Name = "Public Subnet 1d"
   }
 }
 
@@ -65,7 +65,7 @@ resource "aws_subnet" "public_subnet_1e" {
   availability_zone       = var.zone["1e"]
   map_public_ip_on_launch = true
   tags = {
-    Name = "Terraform - Public Subnet 1e"
+    Name = "Public Subnet 1e"
   }
 }
 
@@ -75,7 +75,7 @@ resource "aws_subnet" "public_subnet_1f" {
   availability_zone       = var.zone["1f"]
   map_public_ip_on_launch = true
   tags = {
-    Name = "Terraform - Public Subnet 1f"
+    Name = "Public Subnet 1f"
   }
 }*/
 
@@ -126,8 +126,7 @@ resource "aws_route_table_association" "public_rt_asso_6" {
 
 ######## Private  Resources ######## EIP/ NAT / Subnets / Route Table / Route Table Association
 
-/*
-resource "aws_eip" "eip" {
+/*resource "aws_eip" "eip" {
   tags = { Name = "Terraform - EIP" }
 }
 
@@ -138,41 +137,108 @@ resource "aws_nat_gateway" "nat" {
   tags = {
     Name = "Terraform - NAT"
   }
-}
+}*/
 
-resource "aws_subnet" "private_subnets" {
-  for_each = {
-    "us-east-1a" = var.private_subnet["1"]
-    "us-east-1b" = var.private_subnet["2"]
-    "us-east-1c" = var.private_subnet["3"]
-  }
-
+resource "aws_subnet" "private_subnet_1a" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = each.value
-  availability_zone       = each.key
+  cidr_block              = var.private_subnet["1a"]
+  availability_zone       = var.zone["1a"]
   map_public_ip_on_launch = false
   tags = {
-    Name = "Terraform - Private Subnet: ${each.key}"
+    Name = "Private Subnet 1a"
   }
 }
+
+resource "aws_subnet" "private_subnet_1b" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_subnet["1b"]
+  availability_zone       = var.zone["1b"]
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "Private Subnet 1b"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_subnet["1c"]
+  availability_zone       = var.zone["1c"]
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "Private Subnet 1c"
+  }
+}
+
+/*resource "aws_subnet" "private_subnet_1d" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_subnet["1d"]
+  availability_zone       = var.zone["1d"]
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "Private Subnet 1d"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1e" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_subnet["1e"]
+  availability_zone       = var.zone["1e"]
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "Private Subnet 1e"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1f" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_subnet["1f"]
+  availability_zone       = var.zone["1f"]
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "Private Subnet 1f"
+  }
+}*/
 
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.vpc.id
-  route {
-    nat_gateway_id = aws_nat_gateway.nat.id
-    cidr_block     = "0.0.0.0/0"
-  }
+  #route {
+  #  gateway_id = aws_nat_gateway.nat.id
+  #  cidr_block = "0.0.0.0/0"
+  #}
   tags = {
     Name = "Terraform - Private_RT"
   }
 }
 
 resource "aws_route_table_association" "private_rt_asso_1" {
-  for_each       = aws_subnet.private_subnets
+  subnet_id      = aws_subnet.private_subnet_1a.id
   route_table_id = aws_route_table.private_rt.id
-  subnet_id      = each.value.id
 }
-*/
+
+resource "aws_route_table_association" "private_rt_asso_2" {
+  subnet_id      = aws_subnet.private_subnet_1b.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_rt_asso_3" {
+  subnet_id      = aws_subnet.private_subnet_1c.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+/*resource "aws_route_table_association" "private_rt_asso_4" {
+  subnet_id      = aws_subnet.private_subnet_1d.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_rt_asso_5" {
+  subnet_id      = aws_subnet.private_subnet_1e.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_rt_asso_6" {
+  subnet_id      = aws_subnet.private_subnet_1f.id
+  route_table_id = aws_route_table.private_rt.id
+}*/
 
 ###############
 ## Key-Pairs ##
