03d831338008538f22923bf9f2f0d296411409d4
Ramandeep Singh <reyman.raman@gmail.com>
2025-06-11 17:08:12 +0530

    Updating Public & Private subnets/RT/RTA

--- vpc.tf
+++ vpc.tf
@@ -23,106 +23,40 @@ resource "aws_internet_gateway" "igt" {
   }
 }
 
-resource "aws_subnet" "public_subnet_1a" {
+# Create Public Subnets using for_each
+resource "aws_subnet" "public" {
+  for_each = var.public_subnet
+
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public_subnet["1a"]
-  availability_zone       = var.zone["1a"]
+  cidr_block              = each.value
+  availability_zone       = var.zone[each.key]
   map_public_ip_on_launch = true
+
   tags = {
-    Name = "Public Subnet 1a"
+    Name = "Public Subnet ${each.key}"
   }
 }
 
-resource "aws_subnet" "public_subnet_1b" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public_subnet["1b"]
-  availability_zone       = var.zone["1b"]
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "Public Subnet 1b"
-  }
-}
-
-resource "aws_subnet" "public_subnet_1c" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public_subnet["1c"]
-  availability_zone       = var.zone["1c"]
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "Public Subnet 1c"
-  }
-}
-
-/*resource "aws_subnet" "public_subnet_1d" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public_subnet["1d"]
-  availability_zone       = var.zone["1d"]
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "Public Subnet 1d"
-  }
-}
-
-resource "aws_subnet" "public_subnet_1e" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public_subnet["1e"]
-  availability_zone       = var.zone["1e"]
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "Public Subnet 1e"
-  }
-}
-
-resource "aws_subnet" "public_subnet_1f" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public_subnet["1f"]
-  availability_zone       = var.zone["1f"]
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "Public Subnet 1f"
-  }
-}*/
-
+# Public Route Table
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.vpc.id
+
   route {
-    gateway_id = aws_internet_gateway.igt.id
     cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igt.id
   }
+
   tags = {
-    Name = "Terraform - Public_RT"
+    Name = "Public_RT"
   }
 }
 
-resource "aws_route_table_association" "public_rt_asso_1" {
-  subnet_id      = aws_subnet.public_subnet_1a.id
+# Associate all public subnets with the route table
+resource "aws_route_table_association" "public_rt_association" {
+  for_each       = aws_subnet.public
+  subnet_id      = each.value.id
   route_table_id = aws_route_table.public_rt.id
 }
-
-resource "aws_route_table_association" "public_rt_asso_2" {
-  subnet_id      = aws_subnet.public_subnet_1b.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "public_rt_asso_3" {
-  subnet_id      = aws_subnet.public_subnet_1c.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-/*resource "aws_route_table_association" "public_rt_asso_4" {
-  subnet_id      = aws_subnet.public_subnet_1d.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "public_rt_asso_5" {
-  subnet_id      = aws_subnet.public_subnet_1e.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "public_rt_asso_6" {
-  subnet_id      = aws_subnet.public_subnet_1f.id
-  route_table_id = aws_route_table.public_rt.id
-}*/
 
 # #################### Private  Resources ################### #
 # EIP / NAT / Subnets / Route Table / Route Table Association #
@@ -137,7 +71,7 @@ resource "aws_route_table_association" "public_rt_asso_6" {
 }
 
 resource "aws_nat_gateway" "nat" {
-  subnet_id     = aws_subnet.public_subnet_1a.id
+  subnet_id     = aws_subnet.public["1a"].id
   allocation_id = aws_eip.eip.allocation_id
   depends_on    = [aws_internet_gateway.igt]
   tags = {
@@ -145,104 +79,38 @@ resource "aws_nat_gateway" "nat" {
   }
 }
 
-resource "aws_subnet" "private_subnet_1a" {
+# Create Private Subnets using for_each
+resource "aws_subnet" "private" {
+  for_each = var.private_subnet
+
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.private_subnet["1a"]
-  availability_zone       = var.zone["1a"]
+  cidr_block              = each.value
+  availability_zone       = var.zone[each.key]
   map_public_ip_on_launch = false
+
   tags = {
-    Name = "Private Subnet 1a"
+    Name = "Private Subnet ${each.key}"
   }
 }
 
-resource "aws_subnet" "private_subnet_1b" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.private_subnet["1b"]
-  availability_zone       = var.zone["1b"]
-  map_public_ip_on_launch = false
-  tags = {
-    Name = "Private Subnet 1b"
-  }
-}
-
-resource "aws_subnet" "private_subnet_1c" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.private_subnet["1c"]
-  availability_zone       = var.zone["1c"]
-  map_public_ip_on_launch = false
-  tags = {
-    Name = "Private Subnet 1c"
-  }
-}
-
-resource "aws_subnet" "private_subnet_1d" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.private_subnet["1d"]
-  availability_zone       = var.zone["1d"]
-  map_public_ip_on_launch = false
-  tags = {
-    Name = "Private Subnet 1d"
-  }
-}
-
-resource "aws_subnet" "private_subnet_1e" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.private_subnet["1e"]
-  availability_zone       = var.zone["1e"]
-  map_public_ip_on_launch = false
-  tags = {
-    Name = "Private Subnet 1e"
-  }
-}
-
-resource "aws_subnet" "private_subnet_1f" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.private_subnet["1f"]
-  availability_zone       = var.zone["1f"]
-  map_public_ip_on_launch = false
-  tags = {
-    Name = "Private Subnet 1f"
-  }
-}
-
+# Private Route Table
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.vpc.id
-  #route {
-  #  gateway_id = aws_nat_gateway.nat.id
-  #  cidr_block = "0.0.0.0/0"
-  #}
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+
   tags = {
-    Name = "Terraform - Private_RT"
+    Name = "Private_RT"
   }
 }
 
-resource "aws_route_table_association" "private_rt_asso_1" {
-  subnet_id      = aws_subnet.private_subnet_1a.id
-  route_table_id = aws_route_table.private_rt.id
-}
-
-resource "aws_route_table_association" "private_rt_asso_2" {
-  subnet_id      = aws_subnet.private_subnet_1b.id
-  route_table_id = aws_route_table.private_rt.id
-}
-
-resource "aws_route_table_association" "private_rt_asso_3" {
-  subnet_id      = aws_subnet.private_subnet_1c.id
-  route_table_id = aws_route_table.private_rt.id
-}
-
-resource "aws_route_table_association" "private_rt_asso_4" {
-  subnet_id      = aws_subnet.private_subnet_1d.id
-  route_table_id = aws_route_table.private_rt.id
-}
-
-resource "aws_route_table_association" "private_rt_asso_5" {
-  subnet_id      = aws_subnet.private_subnet_1e.id
-  route_table_id = aws_route_table.private_rt.id
-}
-
-resource "aws_route_table_association" "private_rt_asso_6" {
-  subnet_id      = aws_subnet.private_subnet_1f.id
+# Associate all private subnets with the route table
+resource "aws_route_table_association" "private_rt_association" {
+  for_each       = aws_subnet.private
+  subnet_id      = each.value.id
   route_table_id = aws_route_table.private_rt.id
 }*/
 
@@ -250,32 +118,27 @@ resource "aws_route_table_association" "private_rt_asso_6" {
 ## Key-Pairs ##
 ###############
 
-# --------------------- Bastion Host --------------------- #
 resource "aws_key_pair" "bastion_host" {
   key_name   = "bastion-host"
   public_key = file("key_files/bastion-host.pub")
 }
 
-# --------------------- Web Server --------------------- #
-resource "aws_key_pair" "web01" {
+/*resource "aws_key_pair" "web01" {
   key_name   = "web-host"
   public_key = file("key_files/web01.pub")
 }
 
-# --------------------- Ansible --------------------- #
 resource "aws_key_pair" "ansible" {
   key_name   = "ansible"
   public_key = file("key_files/ansible.pub")
 }
 
-# --------------------- Jenkins Master --------------------- #
 resource "aws_key_pair" "jenkins_master" {
   key_name   = "jenkins_master"
   public_key = file("key_files/jenkins_master.pub")
 }
 
-# --------------------- Jenkins Slave --------------------- #
 resource "aws_key_pair" "jenkins_slave" {
   key_name   = "jenkins_slave"
   public_key = file("key_files/jenkins_slave.pub")
-}
+}*/
