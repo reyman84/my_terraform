59d16bf791b4e0c04585745ab235d1537c738452
Ramandeep Singh <reyman.raman@gmail.com>
2025-06-11 14:26:31 +0530

    Adding S3 bucket & dynamodb tables

--- backend.tf
+++ backend.tf
@@ -1,8 +1,23 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "myterraform-backend2025"  
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name = "Terraform_VPC"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 terraform {
   backend "s3" {
-    bucket = "job-hunt-2025"
+    bucket = "myterraform-backend2025"
     key    = "Terraform_VPC/backend-report"
     region = "us-east-1"
-    #dynamodb_table = "terraform-locks"
+    dynamodb_table = "Terraform_VPC"
   }
 }
