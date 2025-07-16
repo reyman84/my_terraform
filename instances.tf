700531dbd36fec348eeba12d3d643c90dfd4d43f
reyman84 <reyman.raman@gmail.com>
2025-07-16 21:43:17 +0530

    Adding VPC Module

--- instances.tf
+++ instances.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "baston_host" {
   instance_type          = "t2.micro"
   ami                    = var.ami["amazon_linux_2"]
   key_name               = aws_key_pair.devops-project.id
-  subnet_id              = aws_subnet.public_subnet_1a.id
+  subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [aws_security_group.bastion_host.id]
 
   tags = {
@@ -36,7 +36,7 @@ resource "aws_instance" "docker" {
   instance_type = "t2.medium"               # t2-medium is "Chargeable"
   ami           = var.ami["amazon_linux_2"]
   key_name      = aws_key_pair.devops-project.id
-  subnet_id     = aws_subnet.public_subnet_1a.id
+  subnet_id     = module.vpc.public_subnets[1]
   vpc_security_group_ids = [
     aws_security_group.bastion_host.id,
     aws_security_group.http.id
@@ -69,89 +69,13 @@ resource "aws_instance" "docker" {
   }
 }*/
 
-# --------------------- Web Server ---------------------
-/*
-resource "aws_instance" "web_servers" {
-  #for_each      = aws_subnet.private_subnets
-  count         = var.unstable_instance_count
-  ami           = var.ami["amazon_linux_2"]
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.web01.id
-  subnet_id = element([
-    aws_subnet.public_subnet_1a.id,
-    aws_subnet.public_subnet_1b.id,
-    aws_subnet.public_subnet_1c.id
-  ], count.index)
-
-  # This security group should all traffic on port 80
-  vpc_security_group_ids = [
-    aws_security_group.bastion_host.id,
-    aws_security_group.http.id
-  ]
-
-  tags = {
-    Name = "WebServer-${count.index + 1}"
-  }
-
-  user_data = <<-EOF
-                #!/bin/bash
-                sudo yum install wget unzip httpd -y
-                sudo systemctl start httpd
-                sudo systemctl enable httpd
-                wget https://www.tooplate.com/zip-templates/2117_infinite_loop.zip
-                unzip -o 2117_infinite_loop.zip
-                cp -r 2117_infinite_loop /var/www/html/
-                sudo systemctl restart httpd
-                EOF
-}*/
-
-# --------------------- Manual-Project ---------------------
-# --------------------- MYSQL / Mariadb ---------------------
-
-
-/*resource "aws_instance" "manual_roject_sql" {
-  instance_type = "t2.micro"
-  ami           = var.ami["amazon_linux_2"]
-  key_name      = aws_key_pair.devops-project.id
-  subnet_id     = aws_subnet.public_subnet_1a.id
-
-  vpc_security_group_ids = [
-    aws_security_group.bastion_host.id,
-    aws_security_group.http.id
-  ]
-
-  tags = {
-    Name = "Manual_Project_SQL"
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("key-files/devops-project")
-    host        = self.public_ip
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      # Installing MariaDB and GIT
-      "sudo dnf install -y git mariadb105-server",
-      "sudo systemctl start mariadb",
-      "sudo systemctl enable mariadb",
-      "mysql --version",
-      #"sudo systemctl status mariadb",
-      "cd /tmp",
-      "git clone -b local https://github.com/hkhcoder/vprofile-project.git"
-    ]
-  }
-}*/
-
 # --------------------- Ansible Control Machine on Ubuntu ---------------------
 
 /*resource "aws_instance" "ansible_cm" {
   ami                    = var.ami["ubuntu"]
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.ansible.id
-  subnet_id              = aws_subnet.public_subnet_1c.id
+  subnet_id              = module.vpc.public_subnets[2]
   vpc_security_group_ids = [aws_security_group.bastion_host.id]
 
   connection {
@@ -215,8 +139,8 @@ resource "aws_instance" "web_servers" {
 
 locals {
   subnet_id = {
-    "Amazon_Linux-Ansible" = aws_subnet.public_subnet_1a.id
-    "Ubuntu-Ansible"       = aws_subnet.public_subnet_1b.id
+    "Amazon_Linux-Ansible" = module.vpc.public_subnets[1]
+    "Ubuntu-Ansible"       = module.vpc.public_subnets[2]
   }
 }
 
@@ -253,7 +177,7 @@ resource "aws_instance" "jenkins_master" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.small"
   key_name      = aws_key_pair.devops-project.id
-  subnet_id     = aws_subnet.public_subnet_1a.id
+  subnet_id     = module.vpc.public_subnets[0]
 
   vpc_security_group_ids = [
     #aws_security_group.bastion_host.id,
@@ -329,7 +253,7 @@ resource "null_resource" "volume_provisioner" {
   ami           = var.ami["amazon_linux_2"]
   instance_type = "t2.micro"
   key_name      = aws_key_pair.devops-project.id
-  subnet_id     = aws_subnet.public_subnet_1b.id
+  subnet_id     = module.vpc.public_subnets[1]
 
   vpc_security_group_ids = [
     aws_security_group.bastion_host.id,
@@ -404,7 +328,7 @@ resource "aws_instance" "nexus" {
   ami           = data.aws_ami.linux.id
   instance_type = "t2.medium"
   key_name      = aws_key_pair.devops-project.id
-  subnet_id     = aws_subnet.public_subnet_1b.id
+  subnet_id     = module.vpc.public_subnets[1]
 
   vpc_security_group_ids = [
     #aws_security_group.bastion_host.id,
@@ -449,7 +373,7 @@ resource "aws_instance" "sonarqube" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.medium"
   key_name      = aws_key_pair.devops-project.id
-  subnet_id     = aws_subnet.public_subnet_1c.id
+  subnet_id     = module.vpc.public_subnets[2]
 
   vpc_security_group_ids = [
     #aws_security_group.bastion_host.id
