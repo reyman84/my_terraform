acbfbfde4bd26b8c33c6795b78f55fa44bf5ae2a
reyman84 <reyman.raman@gmail.com>
2025-07-16 16:15:27 +0530

    Adding Backend S3

--- backend.tf
+++ backend.tf
@@ -1,8 +1,29 @@
+/*resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "Terraform_VPC"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "Dev"
+  }
+}*/
+
 terraform {
   backend "s3" {
-    bucket = "job-hunt-2025"
-    key    = "Terraform_VPC/backend-report"
+    bucket = "terraform.tfstate2025"
+    key    = "backend-report"
     region = "us-east-1"
-    #dynamodb_table = "terraform-locks"
+    #dynamodb_table = "Terraform_VPC"
+    encrypt = true
   }
 }
