3eabbc2fa8c0b659e75cbe709a00d0e66b116942
reyman84 <reyman.raman@gmail.com>
2025-07-19 20:00:51 +0530

    Jenkins Installation Script Update on Ubuntu

--- scripts/jenkins_master.sh
+++ scripts/jenkins_master.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
-#yum update -y
-#wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-#rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-#yum upgrade -y
-#yum install java-21-amazon-corretto-devel -y
-#yum install jenkins -y
-#systemctl enable jenkins
-#systemctl start jenkins
-#systemctl status jenkins
-#echo "export JAVA_HOME=/usr/lib/jvm/java-21-amazon-corretto.x86_64" >> /etc/profile
-#echo "export PATH=$JAVA_HOME/bin:$PATH" >> /etc/profile
-#source /etc/profile
-#echo $JAVA_HOME
-#java --version
-#sudo yum install -y git
-#sudo hostnamectl set-hostname jenkins-master
-
 sudo apt update
  
-sudo apt install openjdk-21-jdk -y
+sudo apt install openjdk-17-jdk openjdk-21-jdk -y
  
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
 https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
