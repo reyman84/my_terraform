d65ef4d2c1c98ef2e56b84652f6d0c90ac063fb2
Ramandeep Singh <reyman.raman@gmail.com>
2025-11-04 16:01:39 +0530

    Updating deployment stages

--- Devops/Pipelines/Continous_Delivery_pipeline.groovy
+++ Devops/Pipelines/Continous_Delivery_pipeline.groovy
@@ -1,83 +1,162 @@
 pipeline {
     agent any
 
+    tools {
+        maven "myMVN"
+        jdk "myJDK_ubuntu"
+    }
+
     environment {
-        REPO_URL = 'https://github.com/reyman84/terraform-vprofile-project.git'
-        SONAR_SERVER = 'SonarQubeServer'
-        ARTIFACT_REPO = 'Nexus'
+        // Nexus Repositories
+        RELEASE_REPO = 'vprofile-release'       // Maven 2 (hosted) repository
+        SNAP_REPO = 'vprofile-snapshot'         // Maven 2 (hosted) repository
+        CENTRAL_REPO = 'vpro-maven-central'     // Maven 2 (hosted) proxy           https://repo1.maven.org/maven2/
+        NEXUS_GRP_REPO = 'vpro-maven-group'     // Maven 2 (hosted) group
+
+        // Nexus configuration
+        //NEXUS_USER = 'admin'
+        //NEXUS_PASS = 'Khalsa_1699'
+        NEXUSIP = '172.21.2.124'                 // Always change when new servers are launched
+        NEXUSPORT = '8081'
+        NEXUS_LOGIN = 'nexuslogin'
+
+        // SonarQube configuration
+        SONARSCANNER = 'sonarscanner'
+        SONARSERVER = 'sonarserver'
     }
 
     stages {
-        stage('Checkout') {
+        stage('Compile'){
             steps {
-                git branch: 'main', url: "${REPO_URL}"
+                git branch: 'jenkins-ci',
+                url: 'https://github.com/reyman84/vprofile-project.git'
             }
         }
-
-        stage('Build') {
+        stage('Build'){
             steps {
-                sh 'mvn clean package -DskipTests'
+                sh 'mvn -s settings.xml -DskipTests install'
+            }
+            post {
+                success {
+                    echo "Now Archiving the build artifacts"
+                    archiveArtifacts artifacts: '**/*.war'
+                }
             }
         }
 
         stage('Unit Tests') {
             steps {
-                sh 'mvn test'
+                sh 'mvn -s settings.xml test'
             }
         }
 
-        stage('Code Quality - SonarQube') {
+        stage('Checkstyle Analysis') {
             steps {
-                withSonarQubeEnv("${SONAR_SERVER}") {
-                    sh 'mvn sonar:sonar'
+                sh 'mvn -s settings.xml checkstyle:checkstyle'
+            }
+        }
+
+        stage('CODE ANALYSIS with SONARQUBE') {
+            environment {
+                scannerHome = tool "${SONARSCANNER}"
+            }
+            steps {
+                withSonarQubeEnv("${SONARSERVER}") {
+                    sh '''${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=vprofile \
+                    -Dsonar.projectName=vprofile-repo \
+                   -Dsonar.projectVersion=1.0 \
+                   -Dsonar.sources=src/ \
+                   -Dsonar.java.binaries=target/test-classes/com/visualpathit/account/controllerTest/ \
+                   -Dsonar.junit.reportsPath=target/surefire-reports/ \
+                   -Dsonar.jacoco.reportsPath=target/jacoco.exec \
+                   -Dsonar.java.checkstyle.reportPaths=target/checkstyle-result.xml'''
                 }
             }
         }
 
-        stage('Publish Artifact') {
+        stage('SonarQube Quality Gate') {
             steps {
-                sh 'mvn deploy'
+                timeout(time: 10, unit: 'MINUTES') {
+                    waitForQualityGate abortPipeline: true
+                }
             }
         }
 
+        stage('Upload artifact to Nexus') {
+            steps {
+                script {
+                    env.APP_VERSION = "${env.BUILD_ID}-" + sh(
+                        script: "date -u +%Y%m%d%H%M%S",
+                        returnStdout: true
+                        ).trim()
+                        echo "Generated APP_VERSION = ${env.APP_VERSION}"
+                    }
+            nexusArtifactUploader(
+            nexusVersion: 'nexus3',
+            protocol: 'http',
+            nexusUrl: "${NEXUSIP}:${NEXUSPORT}",
+            groupId: 'QA',
+            version: "${APP_VERSION}",   // ✅ now works
+            repository: "${RELEASE_REPO}",
+            credentialsId: "${NEXUS_LOGIN}",
+            artifacts: [[
+                artifactId: 'vproapp',
+                classifier: '',
+                type: 'war',
+                file: 'target/vprofile-v2.war'
+                ]])
+            }
+        }
+           
         stage('Deploy to Staging') {
             steps {
-                echo 'Deploying to staging environment...'
-                sh 'ansible-playbook -i inventory/staging deploy.yml'
+                echo "Deploying version ${APP_VERSION} to staging..."
+                sh """
+                ansible-playbook \
+                -i ansible/inventory/stage \
+                ansible/deploy.yml \
+                --extra-vars "version=${APP_VERSION}"
+                """
             }
         }
 
-        stage('Integration / Acceptance Testing') {
+
+        /*stage('Integration / Acceptance Testing') {
             steps {
                 echo 'Running post-deployment tests on staging...'
                 sh 'pytest tests/integration/'
             }
-        }
+        }*/
 
         stage('Manual Approval') {
             steps {
-                script {
-                    timeout(time: 1, unit: 'HOURS') {
-                        input message: "Approve deployment to PRODUCTION?"
-                    }
+                timeout(time: 1, unit: 'HOURS') {
+                    input message: "Approve deployment to PRODUCTION?",
+                    ok: "Proceed",
+                    submitter: "Ramandeep Singh,admin"
                 }
             }
         }
 
         stage('Deploy to Production') {
             steps {
-                echo 'Deploying to production environment...'
-                sh 'ansible-playbook -i inventory/production deploy.yml'
+                echo "Deploying version ${APP_VERSION} to production..."
+                sh """
+                ansible-playbook \
+                -i ansible/inventory/prod \
+                ansible/deploy.yml \
+                --extra-vars "version=${APP_VERSION}"
+                """
             }
         }
     }
 
-    post {
-        success {
-            echo '✅ Continuous Delivery pipeline executed successfully!'
+    /*post {
+        always {
+            echo "Slack Notification"
+            slackSend channel: '#devops_practices', 
+            color: COLOR_MAP[currentBuild.currentResult],
+            message: "*${currentBuild.currentResult}:* - Job ${env.JOB_NAME} Build ${env.BUILD_NUMBER} \n  More info at: ${env.BUILD_URL}"
         }
-        failure {
-            echo '❌ Pipeline failed. Please check logs.'
-        }
-    }
+    }*/
 }
