f7adc35471f53f31668e0ef9b8a82611419d4fd4
Ramandeep Singh <reyman.raman@gmail.com>
2025-11-09 19:38:26 +0530

    Adding SSH Auth & JAVA Home

--- instances.tf
+++ instances.tf
@@ -134,22 +134,41 @@ resource "aws_instance" "jenkins_slave" {
   }
 
   user_data = <<-EOF
-                #!/bin/bash
+              #!/bin/bash
                 set -eux
                 
-                # Update system
+              # Update system
                 apt update -y
                 apt install -y fontconfig ca-certificates apt-transport-https curl unzip
                 
-                # Install AWS CLI v2
+              # Install AWS CLI v2
                 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
                 unzip awscliv2.zip
                 sudo ./aws/install
                 
-                # Install Java 21
+              # Install Java 21
                 apt install -y openjdk-21-jdk
                 java -version
+
+              # Set JAVA_HOME globally
+                echo 'export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64' | sudo tee -a /etc/profile
+                echo 'export PATH=$PATH:$JAVA_HOME/bin' | sudo tee -a /etc/profile
+
+              # Apply JAVA_HOME for current session
+                export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
+                export PATH=$PATH:$JAVA_HOME/bin
+
+              # Set hostname
                 hostnamectl set-hostname jenkins-slave
+
+              # Enable password authentication for SSH (required for Jenkins Slave connection)
+                sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+                sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config.d/*.conf
+                systemctl restart ssh
+
+                mkdir -p /var/lib/jenkins
+                chown -R ubuntu:ubuntu /var/lib/jenkins
+                sudo chmod 1777 /var/lib/jenkins
             EOF
 }
 
@@ -214,23 +233,31 @@ resource "aws_instance" "jenkins_master" {
   }
 
   user_data = <<-EOF
-                #!/bin/bash
+              #!/bin/bash
                 set -eux
                 
-                # Update system
+              # Update system
                 apt update -y
                 apt install -y fontconfig ca-certificates apt-transport-https curl unzip
                 
-                # Install AWS CLI v2
+              # Install AWS CLI v2
                 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
                 unzip awscliv2.zip
                 sudo ./aws/install
                 
-                # Install Java 21
+              # Install Java 21
                 apt install -y openjdk-21-jdk
                 java -version
+
+              # Set JAVA_HOME globally
+                echo 'export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64' | sudo tee -a /etc/profile
+                echo 'export PATH=$PATH:$JAVA_HOME/bin' | sudo tee -a /etc/profile
+
+              # Apply JAVA_HOME for current session
+                export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
+                export PATH=$PATH:$JAVA_HOME/bin
                 
-                # Install Jenkins repo
+              # Install Jenkins repo
                 mkdir -p /etc/apt/keyrings
                 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key \
                   -o /etc/apt/keyrings/jenkins-keyring.asc
@@ -240,18 +267,24 @@ resource "aws_instance" "jenkins_master" {
                 
                 apt update -y
                 apt install -y jenkins
-                systemctl stop jenkins
                 
-                # Download backup (IAM role must be attached!)
-                # aws s3 cp s3://jenkins-config-terraform/jenkins_configuration.tar.gz /root/jenkins_backup.tar.gz --region us-east-1
+              # Download backup (IAM role must be attached!)
                 aws s3 cp s3://jenkins-config-terraform/jenkins_backup_v1.tar.gz /root/jenkins_backup.tar.gz --region us-east-1
                 
-                # Restore Jenkins config
+              # Restore Jenkins config
+                systemctl stop jenkins
                 tar -xzvf /root/jenkins_backup.tar.gz -C / --overwrite
                 chown -R jenkins:jenkins /var/lib/jenkins
-                
+                rm /root/jenkins_backup.tar.gz
                 systemctl start jenkins
+
+              # Set hostname
                 hostnamectl set-hostname jenkins-master
+
+              # Enable password authentication for SSH (required for Jenkins Slave connection)
+                sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+                sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config.d/*.conf
+                systemctl restart ssh
             EOF
 }
 
